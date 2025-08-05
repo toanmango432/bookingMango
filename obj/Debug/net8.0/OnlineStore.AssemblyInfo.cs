f463707df2971562fdf18c1cb3b0bc17594732a3
khactoan432 <khactoan432@gmail.com>
2025-08-05 17:27:04 +0700

    service v-3

--- obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dffd78458cbdac31f1cc936bfde8dd152089384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a5dc4677d19cc6789eaea85156b0bacb4f1480")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
