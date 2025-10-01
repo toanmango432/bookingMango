480722cb9487e6590ca6e6f3be03e33483e03127
khactoan432 <khactoan432@gmail.com>
2025-10-01 13:33:38 +0700

    create new router

---
+++ Pages/Client-profile.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineStore.Pages
+{
+    public class ClientProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ead2e251a244b4174bff013b8e96fd0278cb8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899927093b2e62cc4090474d20aa4ac205d17616")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
